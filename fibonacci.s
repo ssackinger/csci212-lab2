86f3b94af9f387f93b9638b4f8f23ed2568da705
BuildTools <unconfigured@null.spigotmc.org>
2023-05-23 17:43:31 -0700

    wip

--- fibonacci.s
+++ fibonacci.s
@@ -12,16 +12,38 @@ scan_pattern: .asciz "%d"
 .balign 4
 number_read: .word 0
 
-.balign 4
-return: .word 0
-
 .text
 
+fibonacci:
+    push {lr, r0, r1, r2, r3, r8, r9}
+
+    mov r9, r0          @load passed in value from r0
+    mov r1, #1
+    mov r2, #1
+    mov r3, #0
+    mov r0, #0
+    mov r8, #2
+
+fibonacci_loop:
+    cmp r8, r9
+    bge fibonacci_done
+
+    mov r0, r1
+    add r3, r1, r2
+    mov r1, r2
+    mov r2, r3
+    add r8, r8, #1
+    b fibonacci_loop
+
+fibonacci_done:
+    pop {lr, r0, r1, r2, r3, r8, r9}
+    bx lr
+
+
 .global main
-
-
-
 main:
+    push {lr}
+
     ldr r1, address_of_return
     str lr, [r1]
 
@@ -32,36 +54,21 @@ main:
     ldr r1, address_of_number_read   /* r1 ← &number_read */
     bl scanf                         /* call to scanf */
 
-    mov r1, #1
-    mov r2, #1
-    mov r3, #0
-    mov r0, #0
-    ldr r9, address_of_number_read @arbitrary input for testing
-    ldr r9, [r9]
-    mov r8, #2
+    mov r0, address_of_number_read
+    mov r0, [r0]
 
-fib:
-    cmp r8, r9
-    bge done
+    bl fibonacci
 
-    mov r0, r1
-    add r3, r1, r2
-    mov r1, r2
-    mov r2, r3
-    add r8, r8, #1
-    b fib
-
-done:
+    mov r2, r0                      @load return value into r2 before address_of_format changes it
 
     ldr r0, address_of_format        /* r0 ← &message2 */
     mov r1, r9
-    mov r2, r3
+
     @ldr r1, address_of_number_read   /* r1 ← &number_read */
     @ldr r1, [r1]                     /* r1 ← *r1 */
     bl printf                        /* call to printf */
 
-    ldr lr, address_of_return        /* lr ← &address_of_return */
-    ldr lr, [lr]                     /* lr ← *lr */
+    pop {lr}
     bx lr                            /* return from main using lr */
 
 
@@ -69,8 +76,6 @@ address_of_prompt: .word prompt
 address_of_scan_pattern: .word scan_pattern
 address_of_number_read: .word number_read
 address_of_format: .word format
-address_of_return: .word return
-
 
 .global printf
 .global scanf
